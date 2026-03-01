0dacaf911778e6545ca16dc028f333446ac4e541
Sir Carloz <carlosmaina198@gmail.com>
2026-03-01 10:30:19 +0300

    Merge conflict check

--- j.js
+++ j.js
@@ -1,3 +1,3 @@
-let user = "carlos";
+let user = "Sir carlos";
 let new_task = "Creating a sub-branch for different parts of the system";
 let newtask2 = "|creating a second sub-branch part of the main system *modified 4*"
